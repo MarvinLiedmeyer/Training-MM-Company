478e84638acde1f08a21c27c5a1f26655911671d
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-10 15:01:20 +0200

    :sparkles: Add company2address table

---
+++ Training-MM-Company/Table/Company2Address.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Company2Address]
+(
+	[CompanyId] INT NOT NULL REFERENCES [Company](Id),
+	[AddressId] INT NOT NULL REFERENCES [Address](Id),
+	[CreationTime] DATETIME2,
+	Constraint [PK_Company2Address] PRIMARY KEY ([CompanyId], [AddressId]),
+)
