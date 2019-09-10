397649b8e94cc6624b8e96b67b91405d9471e9f1
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-10 14:56:04 +0200

    :sparkles: Add address table

---
+++ Training-MM-Company/Table/Address.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Street] NVARCHAR(256) ,
+	[City] NVARCHAR(256),
+	[Zip] NVARCHAR(128),
+	[Country] NVARCHAR(128),
+	[DeleteTime] Datetime2 
+)
