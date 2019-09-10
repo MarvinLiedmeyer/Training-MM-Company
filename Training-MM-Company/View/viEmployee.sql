b66f8999f1d245a6541ee85f574d1df08013a75b
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-10 14:54:13 +0200

    :sparkles: Add employee view

---
+++ Training-MM-Company/View/viEmployee.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[viEmployee]
+	AS 
+		SELECT	[Employee].[Id] AS Nummer
+				,[Employee].[FirstName]
+				,[Employee].[LastName]
+				,[Employee].[BeginDate]
+				,[Address].[Id] 
+				,[Address].[Street]
+				,[Address].[City]
+				,[Address].[Zip]
+				,[Address].[Country]
+				,[Department].[Name]
+
+		FROM [Employee]
+		INNER JOIN [Address]
+		ON [Employee].[AddressId] = [Address].[Id] 
+		INNER JOIN [Department]
+		ON [Employee].[DepartmentId] = [Department].[Id] 
+	
