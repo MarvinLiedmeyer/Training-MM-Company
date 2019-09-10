76c77f64e13c653fc0c8a0c45a7e7bf8fc570920
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-10 14:49:29 +0200

    :sparkles: Add company view

---
+++ Training-MM-Company/View/viCompany.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[viCompany]
+	AS 
+		SELECT		[Company].[Id]
+					,[Company].[Name]
+					,[Company].[FoundedDate]
+					,COUNT ([Company2Address].[AddressId]) AS Location
+
+		FROM		[Company]
+					Left JOIN [Company2Address] 
+					ON [Company].[Id] = [Company2Address].[CompanyId]
+		GROUP BY	[Company].[Name], [Company].[Id], [Company].[FoundedDate]
