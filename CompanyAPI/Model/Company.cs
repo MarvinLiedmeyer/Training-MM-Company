ef27b4e8bfa3741491a94f60baf238c38dfe897e
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-13 16:09:11 +0200

    :truck: Import Models

---
+++ CompanyAPI/Model/Company.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConsoleApp.Model {
+    public class Company {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? FoundedDate { get; set; }
+    }
+}
