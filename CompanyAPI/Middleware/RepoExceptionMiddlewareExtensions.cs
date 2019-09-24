7bbb23638804d23c9d22310d5e55ef16981c7ed9
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-24 10:42:17 +0200

    :sparkles: Add RepoExceptionMiddlewareExtensions

---
+++ CompanyAPI/Middleware/RepoExceptionMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace CompanyAPI.Middleware
+{
+    public static class RepoExceptionMiddlewareExtensions
+    {
+        public static void UseRepoExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RepoExceptionMiddleware>();
+        }
+    }
+}
