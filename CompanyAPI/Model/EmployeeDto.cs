49de654a564bc5ea6601ad515f0ef5f33f4b6a5b
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-25 17:17:37 +0200

    :sparkles: Add EmployeeDto Model

---
+++ CompanyAPI/Model/EmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyAPI.Model
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BeginDate { get; set; }
+        public int DepartmentId { get; set; }
+        public int AddressId { get; set; }
+    }
+}
