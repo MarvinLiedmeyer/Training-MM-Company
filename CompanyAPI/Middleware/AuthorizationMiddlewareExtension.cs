3667781c2364c20a1917f3137250583018940d39
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-25 17:09:46 +0200

    :sparkles: Add MiddlewareAuthorization for Extension

---
+++ CompanyAPI/Middleware/AuthorizationMiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace CompanyAPI.Middleware
+{
+    public static class AuthorizationMiddlewareExtension
+    {
+        public static void UseAuthorizationMiddlewareExtension(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<AuthorizationMiddleware>();
+        }
+    }
+}
