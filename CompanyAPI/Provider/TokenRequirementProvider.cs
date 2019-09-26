6bafa9a4deb663f7302b0535d9e9d5d44a1cde16
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-09-26 13:40:12 +0200

    :sparkles: Add TokenRequirementProvider

---
+++ CompanyAPI/Provider/TokenRequirementProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Chayns.Auth.ApiExtensions;
+using Microsoft.AspNetCore.Http;
+
+namespace CompanyAPI.Provider
+{
+    public class TokenRequirementProvider : TokenRequirementProviderBase
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenRequirementProvider(IHttpContextAccessor contextAccessor)
+        {
+            // IHttpContextAccessor will be injected by dependency injection
+            _contextAccessor = contextAccessor;
+        }
+
+        /// <summary>
+        /// Returns the locationId from current url path
+        /// </summary>
+        /// <returns>The locationId that is required</returns>
+        public override Task<int?> GetRequiredLocationId()
+        {
+            // Get parts of current relative path
+            var pathParts = _contextAccessor.HttpContext.Request.Path.Value.Trim('/').Split('/');
+
+            // Schema of api is /api/{locationId}/[controller]
+            var locationIdString = pathParts[1];
+
+            if (int.TryParse(locationIdString, out var locationId))
+                return Task.FromResult((int?)locationId);
+
+            return Task.FromResult(default(int?));
+        }
+
+        /// <summary>
+        /// Not implemented
+        /// </summary>
+        /// <returns></returns>
+        public override Task<string> GetRequiredSiteId() => Task.FromResult(default(string));
+    }
+}
